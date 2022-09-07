cc14bc3625fce54f9c3d8dee03b2b164bca0ea47
MagnumCrab <magnumkrab@gmail.com>
2022-09-07 20:08:09 +0300

    add exercise_9.2

---
+++ exercise_9.2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int m = InputInt("Введите M:");
+int n = InputInt("Введите N:");
+Console.WriteLine($"Сумма элементов от {m} до {n} = {NumbersSum(m, n)}");
+
+int NumbersSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + NumbersSum(m, n - 1);
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
